7dbfdbd3eec4256cb4e3bd94aeb104c51b5eef5d
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-08-24 08:05:28 +0200

    DLIV-34: Added controller for the login page.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    authenticate() {
+      var data = this.getProperties('identification', 'password');
+      return this.get('session').authenticate('authenticator:custom', data);
+    }
+  }
 });
