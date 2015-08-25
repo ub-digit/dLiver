c685d3ed13f78228cfcee7f5db48052a55ce6da6
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-08-25 10:48:17 +0200

    DLIV-35: Added i18n (v4.1.2) and translation drafts.

---
+++ app/locales/en/translations.js
@@ -0,0 +1,29 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  menu: {
+    users: "Users"
+  },
+
+  users: {
+    header: "Users",
+    id: "ID",
+    username: "Username",
+    name: "Name",
+    role: "Role",
+    email: "Email",
+    edit: "Edit",
+    update: "Store changes",
+    cancel: "Cancel",
+    delete: "Delete user",
+    new: "New user",
+    create: "Create user"
+  }
+};
---
+++ app/locales/sv/translations.js
@@ -0,0 +1,29 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  menu: {
+    users: "Användare"
+  },
+
+  users: {
+    header: "Användare",
+    id: "ID",
+    username: "Användarnamn",
+    name: "Namn",
+    role: "Roll",
+    email: "E-post",
+    edit: "Redigera",
+    update: "Spara ändringar",
+    cancel: "Avbryt",
+    delete: "Radera användare",
+    new: "Ny användare",
+    create: "Skapa användare"
+  }
+};
