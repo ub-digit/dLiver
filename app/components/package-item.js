17d7f4b6f0554d4aea804565193d8285e7105515
jkcarlsson85 <jkcarlsson85@gmail.com>
2015-11-26 11:17:27 +0100

    Fix formatting of result list and add pubyear

--- app/components/package-item.js
+++ app/components/package-item.js
@@ -1,25 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['package-item'],
+  classNames: ['package-item', 'clearfix'],
 
   highlights: Ember.computed('package.highlights', function(){
     var highlights = Ember.A([]);
   }),
 
   titleFull: Ember.computed('package.title', 'ordinals', function(){
-    return this.get('package.title') + this.get('ordinals');
+    return this.get('package.title') + " " + this.get('ordinals');
   }),
 
   ordinals: Ember.computed('package.ordinal_1', 'package.ordinal_2', 'package.ordinal_3', function(){
-    var string = "";
+    var array = [this.get('package.ordinal_1'), this.get('package.ordinal_2'), this.get('package.ordinal_3')]
 
-    string += " " + this.get('package.ordinal_1') + " " || "";
-    string += this.get('package.ordinal_2') + " " || "";
-    string += this.get('package.ordinal_3') || "";
-
-    return string;
-
-    //if (this.get('package.ordinal_1'))
+    return array.compact().join(' ');
   })
 });
